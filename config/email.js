b55c705fd547b30289b1b0c7db28b45b788e2326
Brandon Ching <bdching@gmail.com>
2019-09-10 20:47:53 -0700

    How about we not post our API key :facepunch:

--- config/email.js
+++ config/email.js
@@ -2,5 +2,5 @@
 module.exports.email = {
   from: 'Your Name <no-reply@yourapp.com>',
   provider: process.env.EMAIL_PROVIDER || 'SendgridProvider',
-  secret: process.env.EMAIL_SECRET || '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'
+  secret: process.env.EMAIL_SECRET
 };
