70c1f8e3867203205ec82c3b7d390a6a4de0574c
tinhnguyen <tn@ipification>
2019-11-09 20:34:50 +0800

    Update datastores.js

--- config/datastores.js
+++ config/datastores.js
@@ -30,7 +30,7 @@ module.exports.datastores = {
   // You must define env vars for the following Mongo values in a .env file or similar.
   default: {
     adapter: 'sails-mongo',
-    authSource: 'admin',
+    // authSource: 'admin',
     database,
     // replicaSet,
     user, 
